12446b2023519126e59830a17477814561d85142
almir <madarat87@gmail.com>
2022-06-29 19:19:59 +0300

    change retention policy

--- src/main/java/com/example/repos/impl/Singleton.java
+++ src/main/java/com/example/repos/impl/Singleton.java
@@ -1,4 +1,8 @@
 package com.example.repos.impl;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
 public @interface Singleton {
 }
